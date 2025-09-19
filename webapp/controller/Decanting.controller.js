c0039aa0d78bfa66bd24d830a170eb9ecfffeecc
Natalia Florianska <ng6f8y6vkcf3izvtzztwag9e3ncv2b@bots.bitbucket.org>
2025-09-19 14:35:05 +0000

    decanting mockup

--- webapp/controller/Decanting.controller.js
+++ webapp/controller/Decanting.controller.js
@@ -15,12 +15,20 @@ sap.ui.define([
             oSplitAppContainer.toMaster(this.createId("masterProductsDetailsPage"));
             let oBindingContext = oEvent.getParameter('listItem').getBindingContext('data')
             this.getView().byId('masterProductsDetailsPage').setBindingContext(oBindingContext, "data");
-            oSplitAppContainer.toDetail(this.createId("detailSelectBoxTypePage"));
+            const sCurrentAppId = oSplitAppContainer.getCurrentDetailPage().getId();
+            if(sCurrentAppId==this.createId("detailNoItemSelectedPage")){
+                oSplitAppContainer.toDetail(this.createId("detailSelectBoxTypePage"));
+            }
+            
             oModel.setProperty("/selectedItem", oBindingContext.getObject())
         },
         onBackToProductsListPress: function () {
-            let oSplitAppContainer = this.getView().byId('SplitAppDemo')
-            oSplitAppContainer.toMaster(this.createId("masterProductsPage"));
+            let oSplitAppContainer = this.getView().byId('SplitAppDemo');
+            const sCurrentAppId = oSplitAppContainer.getCurrentDetailPage().getId();
+            if(sCurrentAppId!==this.createId("Pack8DivisionTote") && sCurrentAppId!==this.createId("DetailPackOtherPage") ){
+                oSplitAppContainer.toMaster(this.createId("masterProductsPage"));
+            }
+            
 
         },
         onToteSelected: function (oEvent) {
@@ -38,7 +46,68 @@ sap.ui.define([
             const sPath = oBindingContext.getPath();
             let oModel = this.getView().getModel('data');
             oModel.setProperty(sPath+"/status", "selected");
-            let a = 1;
+            oEvent.getSource().getParent().getParent().addStyleClass("readyForPackingBackground");
+        },
+        onCloseTote: function(oEvent){
+            let oButton = oEvent.getSource();
+            let sKey = oButton.getCustomData()[0].getValue();
+            let oBindingContext = oButton.getBindingContext('data');
+           
+            const sPath = oBindingContext.getPath();
+            let oModel = this.getView().getModel('data');
+            
+            let iAmountOfItems = oButton.getParent().getItems()[2].getValue();
+
+            if(iAmountOfItems==0){
+                return;
+            }
+            oModel.setProperty(sPath+"/status", "closed");
+            oModel.setProperty(sPath+"/itemId", oModel.getProperty("/selectedItem/itemId"));
+            oModel.setProperty(sPath+"/itemDescription", oModel.getProperty("/selectedItem/material/description"));
+             oButton.getParent().getParent().removeStyleClass("readyForPackingBackground");
+             oButton.getParent().getParent().addStyleClass("packedBackground");
+             if(iAmountOfItems >= oModel.getProperty("/selectedItem/quantity/amount2")){
+                let oSplitAppContainer = this.getView().byId('SplitAppDemo');
+                oSplitAppContainer.toMaster(this.createId("masterProductsPage"));
+                let oListProduct = this.getView().byId("listProducts");
+                let aItems = oListProduct.getItems();
+                for(let i = 0; i< aItems.length; i++){
+                    let oBindingContext = aItems[i].getBindingContext('data');
+                    if(oBindingContext.getProperty('itemId') == oModel.getProperty("/selectedItem/itemId")){
+                        aItems[i].addStyleClass("packedBackground")
+                    }
+                }
+             }
+           //  let oStepInput = this.getView().byId().
+             //StepInputProductId
+
+        },
+        onPressPrintLabel: function(){
+
+        },
+        onCompleteTotePress: function(oEvent){
+            let oSplitAppContainer = this.getView().byId('SplitAppDemo');
+            const sCurrentAppId = oSplitAppContainer.getCurrentDetailPage().getId();
+            sap.ui.getCore().byId(sCurrentAppId).rerender();
+            let oModel = this.getView().getModel('data');
+            let aTotes = oModel.getProperty('/totes');
+            let aTotesList = this.getView().byId('tote8Grid')?.getItems();
+            if(!aTotesList){
+                return;
+            }
+            for(let i = 0; i< aTotes.length; i++){
+                oModel.setProperty("/totes/"+i+"/status", "notselected");
+                aTotesList[i].removeStyleClass("readyForPackingBackground");
+                aTotesList[i].removeStyleClass("packedBackground");
+            }
+
+            oSplitAppContainer.toDetail(this.createId("detailSelectBoxTypePage"));            
+        },
+        onBackToSelectTotePress: function(){
+            let oSplitAppContainer = this.getView().byId('SplitAppDemo');          
+            
+                oSplitAppContainer.toDetail(this.createId("detailSelectBoxTypePage"));
+            
         }
     });
 });
