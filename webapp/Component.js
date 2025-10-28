afd1eb60af66ded74d9b83d43fbcb5a842a0f403
Manik Saluja <manik.saluja@westernacher.com>
2025-10-28 10:06:59 +0000

    changes for the decice type added

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "com/westernacher/decanting/model/models",
-     "./controller/ErrorHandler",
-], (UIComponent, models,ErrorHandler) => {
+    "./controller/ErrorHandler",
+     "sap/ui/Device",
+], (UIComponent, models, ErrorHandler,Device) => {
     "use strict";
 
     return UIComponent.extend("com.westernacher.decanting.Component", {
@@ -24,6 +25,35 @@ sap.ui.define([
             this.getRouter().initialize();
             //this.oErrorHandler = new ErrorHandler(this);
 
-        }
+        },
+        getContentDensityClass: function () {
+            if (this._sContentDensityClass === undefined) {
+                // check whether FLP has already set the content density class; do nothing in this case
+                var element = document.getElementsByTagName("body")[0];
+                if (element.classList.contains("sapUiSizeCozy") || element.classList.contains("sapUiSizeCompact")) {
+                    this._sContentDensityClass = "";
+                } else if (Device.system.desktop && Device.support.touch) { // apply "compact" mode if touch is not supported
+                    // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                } else if (Device.support.touch) { // apply "compact" mode if touch is not supported
+                    // "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+                    this._sContentDensityClass = "sapUiSizeCozy";
+                } else {
+                    //sapUiSizeCompact
+                    this._sContentDensityClass = "sapUiSizeCompact";
+                }
+            }
+            return this._sContentDensityClass;
+        },
+
+        /**
+         * check if mobile device
+         * @returns {boolean}
+         * @private
+         */
+        isMobile: function () {
+            return Device.system.tablet || Device.system.phone;
+        },
+
     });
 });
